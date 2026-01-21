8406d464bcb2ea3942dd9bf210383d24b64d2e93
igorpardinho <pardinhoigor@gmail.com>
2026-01-21 19:36:48 -0300

    feat: adiciona a rota(ong) no app

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import openapi from "@elysiajs/openapi";
 import { petRoutes } from "./modules/pets/pet.routes";
 import { userRoutes } from "./modules/users/user.routes";
 import { errorHandler } from "./shared/plugins/error-handler";
+import { ongRoutes } from "./modules/ong/ong.routes";
 
 export const app = new Elysia()
     .use(openapi())
     .use(errorHandler)
     .use(healthRoutes)
     .use(petRoutes)
-    .use(userRoutes);
+    .use(userRoutes)
+    .use(ongRoutes);
